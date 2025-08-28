3e00d3756d9d28006c0844e5c42720e28cc22339
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2025-08-28 19:33:43 +0800

    added config and migration

---
+++ Persistence/Configurations/AttendanceConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Configurations;
+
+internal sealed class AttendanceConfiguration : IEntityTypeConfiguration<Attendance>
+{
+    public void Configure(EntityTypeBuilder<Attendance> builder)
+    {
+        builder.Property(a => a.PatientId)
+            .IsRequired();
+
+        builder.Property(a => a.DoctorId)
+            .IsRequired();
+
+        builder.HasOne<Patient>()
+            .WithMany()
+            .HasForeignKey(p => p.PatientId)
+            .OnDelete(DeleteBehavior.Restrict); // need account enabled/disabled behaviour
+
+        builder.HasOne<Doctor>()
+            .WithMany()
+            .HasForeignKey(p => p.DoctorId)
+            .OnDelete(DeleteBehavior.Restrict); // need account enabled/disabled behaviour
+    }
+}
---
+++ Persistence/Migrations/20250828113229_Attendance_Config.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Attendance_Config : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_DoctorId",
+                table: "Attendances",
+                column: "DoctorId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Attendances_PatientId",
+                table: "Attendances",
+                column: "PatientId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_Doctors_DoctorId",
+                table: "Attendances",
+                column: "DoctorId",
+                principalTable: "Doctors",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Attendances_Patients_PatientId",
+                table: "Attendances",
+                column: "PatientId",
+                principalTable: "Patients",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_Doctors_DoctorId",
+                table: "Attendances");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Attendances_Patients_PatientId",
+                table: "Attendances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attendances_DoctorId",
+                table: "Attendances");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Attendances_PatientId",
+                table: "Attendances");
+        }
+    }
+}
