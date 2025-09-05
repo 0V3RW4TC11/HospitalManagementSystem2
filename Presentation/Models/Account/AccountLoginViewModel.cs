a36f1e0b3b9e71c873db0030172717fc8a89b03f
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2025-09-05 22:10:12 +0800

    namespace references simplified

--- Presentation/Models/Account/AccountLoginViewModel.cs
+++ Presentation/Models/Account/AccountLoginViewModel.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Presentation.Models
+namespace Presentation.Models.Account
 {
-    public class LoginViewModel
+    public class AccountLoginViewModel
     {
         [Required]
         [EmailAddress]
