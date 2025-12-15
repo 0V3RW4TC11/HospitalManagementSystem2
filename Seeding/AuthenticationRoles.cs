dbc150baa85b5b00fad5f75b4ce7d1f806b4ac00
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2025-12-16 06:28:59 +0800

    wip

---
+++ Seeding/AuthenticationRoles.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Seeding
+{
+    internal static class AuthenticationRoles
+    {
+        public static async Task Seed(RoleManager<IdentityRole> roleManager)
+        {
+            foreach (var roleName in Constants.AuthRoles.AsList())
+            {
+                if (await roleManager.RoleExistsAsync(roleName))
+                {
+                    continue;
+                }
+
+                await roleManager.CreateAsync(new() { Name = roleName });
+            }
+        }
+    }
+}
---
+++ Seeding/Specializations.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Exceptions;
+using Services.Abstractions;
+
+namespace Seeding
+{
+    internal static class Specializations
+    {
+        public static async Task Seed(ISpecializationService service, string pathToCsv)
+        {
+            foreach (var line in File.ReadLines(pathToCsv))
+            {
+                try
+                {
+                    await service.CreateAsync(new() { Name = line.Trim() });
+                }
+                catch (SpecializationDuplicationException)
+                {
+                }
+            }
+        }
+    }
+}
