ea24161176d9f05b2cb2d5c4460560914ba61680
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2025-12-17 21:32:12 +0800

    wip update 3

--- Seeding/Seeders/AdminsSeeder.cs
+++ Seeding/Seeders/AdminsSeeder.cs
@@ -2,18 +2,11 @@
 using Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Seeding.Seeders
 {
     internal static class AdminsSeeder
     {
-        private static readonly string[] _gender = ["Male", "Female"];
-
         public static async Task SeedAsync(
             IDbContextFactory<RepositoryDbContext> factory,
             int totalUsers,
@@ -28,15 +21,48 @@ namespace Seeding.Seeders
                 return;
             }
 
+            int batches = (totalUsers + batchSize - 1) / batchSize;
+            var faker = GetAdminFaker();
 
+            await Parallel.ForEachAsync(
+                Enumerable.Range(0, batches),
+                new ParallelOptions { MaxDegreeOfParallelism = parallelism },
+                async (batchIndex, ct) =>
+                {
+                    var adminsInBatch = Math.Min(batchSize, totalUsers - batchIndex * batchSize);
+                    var admins = faker.Generate(adminsInBatch);
+                    await using var db = await factory.CreateDbContextAsync(ct);
+
+                    db.Admins.AddRange(admins);
+                    await db.SaveChangesAsync(ct);
+                    Console.WriteLine($"Seeded batch {batchIndex + 1}/{batches}. {adminsInBatch} in batch.");
+                });
+
+            Console.WriteLine("Admin seeding complete.");
         }
 
         private static Faker<Admin> GetAdminFaker()
         {
-            var faker = new Faker<Admin>()
-                .RuleFor(a => a.Gender, f => f.PickRandom(_gender))
-                .RuleFor(a => a.FirstName, f => f.Name.FirstName())
-                .RuleFor(a => a.FirstName, f => f.Name.FirstName())
+            return new Faker<Admin>()
+                .CustomInstantiator(f =>
+                {
+                    var genderEnum = f.PickRandom<Bogus.DataSets.Name.Gender>();
+                    var firstName = f.Name.FirstName(genderEnum);
+                    var lastName = f.Name.LastName(genderEnum);
+                    var gender = genderEnum == Bogus.DataSets.Name.Gender.Male ? "Male" : "Female";
+
+                    return new Admin
+                    {
+                        Title = f.Name.Prefix(genderEnum),
+                        FirstName = firstName,
+                        LastName = lastName,
+                        Gender = gender,
+                        Address = f.Address.FullAddress(),
+                        Phone = f.Phone.PhoneNumber(),
+                        Email = f.Internet.Email(firstName, lastName),
+                        DateOfBirth = DateOnly.FromDateTime(f.Person.DateOfBirth)
+                    };
+                });
         }
     }
 }
