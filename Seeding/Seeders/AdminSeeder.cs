a7ef91f26829021f1265400c13c3fb680420db81
0V3RW4TC11 <102503090+0V3RW4TC11@users.noreply.github.com>
2025-12-18 23:04:16 +0800

    wip update 5

---
+++ Seeding/Seeders/AdminSeeder.cs
@@ -0,0 +1,59 @@
+﻿using Bogus;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+using Seeding.Helpers;
+using static Bogus.DataSets.Name;
+
+namespace Seeding.Seeders
+{
+    internal class AdminSeeder
+    {
+        private readonly Faker<Admin> _faker;
+
+        public AdminSeeder()
+        {
+            _faker = new Faker<Admin>()
+                .RuleFor(a => a.Gender, f => f.PickRandom<Gender>() == Gender.Male ? "Male" : "Female")
+                .RuleFor(a => a.Title, (f, a) => f.Name.Prefix(GetGender(a.Gender!)))
+                .RuleFor(a => a.FirstName, (f, a) => f.Name.FirstName(GetGender(a.Gender!)))
+                .RuleFor(a => a.LastName, (f, a) => f.Name.LastName(GetGender(a.Gender!)))
+                .RuleFor(a => a.Address, f => f.Address.FullAddress())
+                .RuleFor(a => a.Phone, f => f.Phone.PhoneNumber())
+                .RuleFor(a => a.Email, (f, a) => f.Internet.Email(a.FirstName, a.LastName))
+                .RuleFor(a => a.DateOfBirth, f => DateOnly.FromDateTime(f.Person.DateOfBirth));
+        }
+
+        public async Task SeedAsync(
+            IDbContextFactory<RepositoryDbContext> factory,
+            int totalAdmins,
+            int batchSize,
+            int parallelism)
+        {
+            if (await DatabaseHelper.HasDataAsync<Admin>(factory))
+            {
+                Console.WriteLine("Admins already populated.");
+                return;
+            }
+
+            await ParallelSeeder.SeedAsync<Admin>(
+                async (adminsToBatch, ct) =>
+                {
+                    // EXAMPLE!
+                    // TODO: Use batching into Admins table and Identity framework core bulk extensions for better performance
+                    var admins = _faker.Generate(adminsToBatch);
+                    await using var db = await factory.CreateDbContextAsync(ct);
+                    db.Set<Admin>().AddRange(admins);
+                    await db.SaveChangesAsync(ct);
+                },
+                totalAdmins,
+                batchSize,
+                parallelism);
+
+            Console.WriteLine("Admin seeding completed.");
+        }
+
+        private static Gender GetGender(string gender) =>
+            gender == "Male" ? Gender.Male : Gender.Female;
+    }
+}
--- Seeding/Seeders/ParallelSeeder.cs
+++ Seeding/Seeders/ParallelSeeder.cs
@@ -1,43 +1,28 @@
 ﻿using Bogus;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Seeding.Seeders
 {
     internal static class ParallelSeeder
     {
         public static async Task SeedAsync<T>(
-            IDbContextFactory<RepositoryDbContext> factory,
-            Faker<T> faker,
+            Func<int, CancellationToken, Task> insertActionAsync,
             int totalEntities,
             int batchSize,
-            int parallelism
-            ) where T : class
+            int parallelism) where T : class
         {
-            using var context = await factory.CreateDbContextAsync();
-            bool exists = await context.Set<T>().AnyAsync();
-            if (exists)
-            {
-                Console.WriteLine($"{nameof(T)}s already seeded");
-                return;
-            }
-
             int batches = (totalEntities + batchSize - 1) / batchSize;
             
             await Parallel.ForEachAsync(
-                Enumerable.Range(0, batches)),
+                Enumerable.Range(0, batches),
                 new ParallelOptions { MaxDegreeOfParallelism = parallelism },
                 async (batchIndex, ct) =>
                 {
-                    var entitiesInBatch = Math.Min(batchSize, totalUsers - batchIndex * batchSize);
-                    var entities = faker.Generate(entitiesInBatch);
-
-                }
+                    var entitiesInBatch = Math.Min(batchSize, totalEntities - batchIndex * batchSize);
+                    await insertActionAsync(entitiesInBatch, ct);
+                    Console.WriteLine($"Seeded batch {batchIndex + 1}/{batches}. {entitiesInBatch} in batch.");
+                });
         }
     }
 }
